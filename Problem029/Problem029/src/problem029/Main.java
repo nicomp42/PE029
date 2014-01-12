491b3fcc3170c2b0406ebfa5b8bb4c3bb8d16604
Bill Nicholson <GitHub@NicholsonComputerConsulting.com>
2014-01-11 19:49:36 -0500

    Working Version

---
+++ Problem029/Problem029/src/problem029/Main.java
@@ -0,0 +1,19 @@
+// Problem 029 Main
+// http://projecteuler.net/problem=29
+
+package problem029;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		Problem029 problem029 = new Problem029();
+		
+		System.out.println("Problem 029: " + problem029.Solve());
+		
+	}
+
+}
